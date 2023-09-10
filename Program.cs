dd573210fa0b1b59e6e684be37c5a0d880f37c40
Ruben <rubensaraldaodevelopper@outlook.com>
2023-09-10 02:22:57 +0200

    Add DIOW project.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Timers;
+
+namespace WiFiMonitor
+{
+    class Program
+    {
+        private static System.Timers.Timer checkTimer;
+        private static HashSet<string> knownMACAddresses = new HashSet<string>();
+
+        static void Main()
+        {
+            checkTimer = new System.Timers.Timer(15000);
+            checkTimer.Elapsed += CheckForNewDevices;
+            checkTimer.Start();
+
+            Console.WriteLine("Monitoring connected devices...");
+            Console.ReadLine();
+        }
+
+        private static void CheckForNewDevices(object sender, ElapsedEventArgs e)
+        {
+            var currentDevices = GetConnectedDevices();
+            foreach (var mac in currentDevices)
+            {
+                if (!knownMACAddresses.Contains(mac))
+                {
+                    Console.WriteLine($"New device detected: {mac}");
+                    knownMACAddresses.Add(mac);
+                }
+            }
+        }
+
+        private static HashSet<string> GetConnectedDevices()
+        {
+            HashSet<string> macAddresses = new HashSet<string>();
+            var arpStream = ExecuteCommandLine("arp", "-a");
+
+            while (!arpStream.EndOfStream)
+            {
+                var line = arpStream.ReadLine()?.Trim();
+                if (line != null && (line.StartsWith("192.168.") || line.StartsWith("10.")))
+                {
+                    var parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length >= 2)
+                    {
+                        var mac = parts[1];
+                        macAddresses.Add(mac);
+                    }
+                }
+            }
+
+            return macAddresses;
+        }
+
+        private static StreamReader ExecuteCommandLine(string file, string args = "")
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = file,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+
+            return process.StandardOutput;
+        }
+    }
+}
